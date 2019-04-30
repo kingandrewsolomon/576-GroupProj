ebdb65baa03fc4ee2474ffb1b09acdce7e755201
Gabe Barrera <gsbarrera1@gmail.com>
2019-04-30 09:17:34 -0700

    comments

--- stream_server.js
+++ stream_server.js
@@ -2,35 +2,40 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const favicon = require('serve-favicon')
-
 const port = 80;
 const app = express();
 const router = express.Router();
+const favicon = require('serve-favicon')
 
+// Root
 router.get('/', (req, res) => {
     console.log("receiving request");
     res.sendFile(path.join(__dirname + '/content/index.html'));
 });
 
+// Video Tab
 router.get('/video', (req, res) => {
     console.log("video page");
     res.sendFile(path.join(__dirname + '/content/video.html'));
 });
 
+// Music Tab
 router.get('/music', (req, res) => {
     console.log("music page");
     res.sendFile(path.join(__dirname + '/content/music.html'));
 });
 
+// Streaming function
 router.get('/stream', (req, res) => {
     console.log(req.url);
     let t = req.url;
 
+    // Default Value
     if (t.indexOf('?') === -1) {
         t = '/stream?movie=MeshNet.mov';
     }
 
+    // Retrieve requested media file
     t = t.split("?");
     t.splice(0, 1);
     let video = t[0].split('=')[1];
@@ -68,10 +73,7 @@ router.get('/stream', (req, res) => {
     }
 });
 
-app.get('/content/*', function (req, res) {
-    res.sendFile(path.join(__dirname + req.url));
-});
 app.use('/', router);
-app.use('/public', express.static('public'))
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use('/public', express.static('public'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}`));
